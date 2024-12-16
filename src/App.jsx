7244934882a5dfd76287840ff69e2136b53c2b54
Muhammad Waleed Mughal <mwm23101@gmail.com>
2024-12-16 23:14:19 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from './components/Hero.jsx'
+
+const App = () => {
+  return (
+    <main className="relitive min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+    </main>
+  )
+}
+
+export default App
